8bdbea2cdca1eb7511bad41fc827b288e533eeb3
teebin2019 <teebin2019@gmail.com>
2025-10-05 12:43:00 +0700

    update router

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -1,18 +1,23 @@
 import iconTeerodev from "../assets/teedev.jpg";
+import { NavLink } from "react-router";
 
 export default function Appbar() {
   const mainItemNav = [
     {
       title: "About me",
+      href: "about",
     },
     {
       title: "Skills",
+      href: "skill",
     },
     {
       title: "Portfolio",
+      href: "portfolio",
     },
     {
       title: "Contact Me",
+      href: "contact",
     },
   ];
 
@@ -53,12 +58,12 @@ export default function Appbar() {
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white ">
             {mainItemNav.map((item, idx) => (
               <li key={idx}>
-                <a
-                  href="#"
+                <NavLink
+                  to={item.href}
                   className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
                 >
                   {item.title}
-                </a>
+                </NavLink>
               </li>
             ))}
           </ul>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+import About from "./About.jsx";
+import Skill from "./Skill.jsx";
+import Portfolio from "./Portfolio.jsx";
+import Contact from "./Contact.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />} />
+      <Route path="about" element={<About />} />
+      <Route path="skill" element={<Skill />} />
+      <Route path="portfolio" element={<Portfolio />} />
+      <Route path="contact" element={<Contact />} />
+    </Routes>
+  </BrowserRouter>
+);
