e62705ce32953b18516ca7a0ea2d14eedf5503fc
teebin2019 <teebin2019@gmail.com>
2025-10-07 13:58:41 +0700

    update users

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,4 +1,23 @@
+import { useState, useEffect } from "react";
+
 export default function Users() {
+  const [users, setUsers] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/api/users");
+      const result = await response.json();
+      console.log(result);
+      setUsers(result.users);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="p-4 sm:ml-64">
       <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg ">
@@ -7,53 +26,45 @@ export default function Users() {
             <thead className="text-xs text-gray-700 uppercase bg-gray-50  ">
               <tr>
                 <th scope="col" className="px-6 py-3">
-                  Product name
+                  ชื่อ-นามสกุล
                 </th>
                 <th scope="col" className="px-6 py-3">
-                  Color
+                  อีเมล
                 </th>
                 <th scope="col" className="px-6 py-3">
-                  Category
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Price
+                  การดำเนินการ
                 </th>
               </tr>
             </thead>
             <tbody>
-              <tr className="bg-white border-b   border-gray-200">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-                >
-                  Apple MacBook Pro 17"
-                </th>
-                <td className="px-6 py-4">Silver</td>
-                <td className="px-6 py-4">Laptop</td>
-                <td className="px-6 py-4">$2999</td>
-              </tr>
-              <tr className="bg-white border-b   border-gray-200">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-                >
-                  Microsoft Surface Pro
-                </th>
-                <td className="px-6 py-4">White</td>
-                <td className="px-6 py-4">Laptop PC</td>
-                <td className="px-6 py-4">$1999</td>
-              </tr>
-              <tr className="bg-white ">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-                >
-                  Magic Mouse 2
-                </th>
-                <td className="px-6 py-4">Black</td>
-                <td className="px-6 py-4">Accessories</td>
-                <td className="px-6 py-4">$99</td>
-              </tr>
+              {users.length > 0 ? (
+                users.map((user) => (
+                  <tr
+                    className="bg-white border-b   border-gray-200"
+                    key={user.id}
+                  >
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                    >
+                      {user.first_name} {user.last_name}
+                    </th>
+                    <td className="px-6 py-4">{user.email}</td>
+                    <td className="px-6 py-4 ">
+                      <a
+                        href="#"
+                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                      >
+                        Edit
+                      </a>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr className="bg-white border-b   border-gray-200" >
+                  <td className="px-6 py-4 text-center " colspan="3">ไม่มีข้อมูล</td>
+                </tr>
+              )}
             </tbody>
           </table>
         </div>
